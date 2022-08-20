44e6f3a725a0eccaf417221589fb63d2533ecc0a
Hannah Kolbeck <hskolbeck@gmail.com>
2022-08-20 09:08:14 -0700

    Handle no text extracted better

--- src/ocr.js
+++ src/ocr.js
@@ -15,12 +15,15 @@ async function ocr(url) {
             return []
         });
     if (result && result.textAnnotations) {
-        console.log("textAnnotations: " + JSON.stringify(result.textAnnotations))
         const text = result.textAnnotations
             .filter(t => !!t.locale)
             .map(t => t.description)
             .join(" ")
             .replace(/(\r\n|\n|\r)/gm, " ");
+
+        if (!text) {
+            return null
+        }
 
         const locales = result.textAnnotations
             .filter(t => !!t.locale)
